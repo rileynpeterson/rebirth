6ba62882314ad03c47b69ea270beb257caf2b791
rileynpeterson <rpeterson114@gmail.com>
2021-03-28 23:38:57 -0400

    Main menu / pause menu
    I edited what Lily did for the main menu and then created a pause menu when esc is pressed. Some buttons do not work (ie About). Quit uses the debug log to demonstrate to us on unity that it is working, but won't actually quit the program unless we create a build. :)

---
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Scripts/UI/MainMenuRiley.cs
@@ -0,0 +1,27 @@
+﻿//written by Riley Peterson 3/28/2021
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenuRiley : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+}
---
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Scripts/UI/PauseMenuRiley.cs
@@ -0,0 +1,59 @@
+﻿//written by Riley Peterson 3/28/2021
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuRiley : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    //Resume function
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //resumes time
+        GameIsPaused = false;
+    }
+
+    //Pause function
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //pauses time
+        GameIsPaused = true;
+    }
+
+    //load menu function
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f; //resume time
+        SceneManager.LoadScene("Menu");
+    }
+
+    //quit function
+    public void QuitMenu()
+    {
+        Debug.Log("Quitting Game.");
+        Application.Quit();
+    }
+
+}
