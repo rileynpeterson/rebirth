4649897549b842841f2b1790aa0970e27e6ff4a0
HandyD <dorceus.handy@gmail.com>
2021-03-30 02:02:24 -0400

    Inventory
    Tried to add an inventory to the UI and collectible items

---
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Scripts/ItemContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemContainer : MonoBehaviour {
+
+      public GameHandler gameController;
+      public string ItemName;
+
+      void Awake(){
+            GameObject gameControllerLocation = GameObject.FindWithTag("GameController");
+            if (gameControllerLocation != null) {
+                  gameController = gameControllerLocation.GetComponent<GameHandler>();
+            }
+      }
+
+      void OnTriggerEnter2D(Collider2D coll){
+            if (coll.tag == "Player"){
+                  //Debug.Log("You found an" + ItemName);
+                  gameController.InventoryAdd(ItemName);
+                  //gameController.removeObjectFromLevel(ItemName);
+                  Destroy(gameObject);
+            }
+      }
+}
