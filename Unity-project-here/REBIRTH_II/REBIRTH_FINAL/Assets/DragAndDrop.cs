d0688049f3ecaeaa6c165a936b7e35bd76676a4a
ltran13 <80127668+ltran13@users.noreply.github.com>
2021-04-04 21:45:41 -0400

    finished drag and drop mouse controls

---
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/DragAndDrop.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*
+public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    //private CanvasGroup canvasGroup;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        //canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+        //canvas.scaleFactor; //movement 
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+}
+*/
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject correctForm;
+    private bool moving;
+
+    private float startPosX;
+    private float startPosY;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y -startPosY, this.gameObject.transform.localPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            moving = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+    }
+}
