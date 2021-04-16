3fbb6b8080948e06cfaa673db4bd9493deecd4e2
ltran13 <80127668+ltran13@users.noreply.github.com>
2021-04-16 02:05:21 -0400

    added remote manual icon and made it clickable

---
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Popup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject remotePanel;
+
+    void Start()
+    {
+        remotePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Clicked_remote()
+    {
+
+        remotePanel.SetActive(!remotePanel.activeSelf);
+        if (Time.timeScale != 0f)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        
+    }
+}
