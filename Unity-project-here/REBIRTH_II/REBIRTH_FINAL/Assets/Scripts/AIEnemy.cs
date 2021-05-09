cb2deca177d0b2b0b075dc4bc1fcd16ad3285ac9
ltran13 <80127668+ltran13@users.noreply.github.com>
2021-05-09 01:34:28 -0400

    shooting enemy

--- Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Scripts/AIEnemy.cs
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Scripts/AIEnemy.cs
@@ -4,18 +4,90 @@ using UnityEngine;
 
 public class AIEnemy : MonoBehaviour
 {
-    public float speed;
-    public float stopping;
+    public Rigidbody2D rb;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+    public Transform player;
+    public Transform shootPos;
+    public GameObject bullet;
+
+    public float walkSpeed, range, timeShots, shootSpeed;
+    private float distToPlayer;
+
+    public bool mustPatrol, canShoot;
+    private bool mustPatrolTurn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mustPatrol = true;
+        canShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+        distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distToPlayer <= range)
+        {
+            if(player.position.x > transform.position.x  && transform.localScale.x < 0 
+                || player.position.x < transform.position.x && transform.localScale.x > 0)
+            {
+                Flip();
+            }
+            mustPatrol = false;
+            rb.velocity = Vector2.zero;
+
+            if(canShoot = true)
+            {
+
+                StartCoroutine(Shoot());
+            }
+        }
+        else
+        {
+            mustPatrol = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (mustPatrol)
+        {
+            mustPatrolTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+        }
+    }
+
+    void Patrol()
+    {
+        if (mustPatrolTurn || bodyCollider.IsTouchingLayers(groundLayer))
+        {
+            Flip();
+        }
+
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(timeShots);
+        GameObject newBullet = Instantiate(bullet, shootPos.position, Quaternion.identity);
+
+        newBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * walkSpeed * Time.fixedDeltaTime, 0f);
+        canShoot = true;
+
     }
 }
---
+++ Unity-project-here/REBIRTH_II/REBIRTH_FINAL/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float dieTime, damage;
+    public GameObject dieEffect;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Die();
+    }
+    IEnumerator CountDownTimer()
+    {
+        yield return new WaitForSeconds(dieTime);
+
+        Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
